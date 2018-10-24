8a4de17abb469391af86e88a1201825d21749a06
Ronie Salgado <roniesalg@gmail.com>
2018-10-24 05:44:57 -0300

    Adding some configuration C files.

---
+++ configuration/pthreads.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define SIZEOF_IN_WORDS(typename) (int)((sizeof(typename) + sizeof(void*) - 1) / sizeof(void*))
+
+#define TYPE_INFO(typename) \
+    printf("alias %s := UIntPointer array: %d. \"sizeof(%s) = %d\"\n", #typename, (int)SIZEOF_IN_WORDS(typename), #typename, (int) sizeof(typename));
+
+#define CONSTANT_INT(constName) \
+    printf("alias %s := %d.\n", #constName, (int)constName);
+
+int main()
+{
+    TYPE_INFO(pthread_attr_t);
+    TYPE_INFO(pthread_mutex_t);
+    TYPE_INFO(pthread_mutexattr_t);
+    TYPE_INFO(pthread_cond_t);
+    TYPE_INFO(pthread_condattr_t);
+    TYPE_INFO(pthread_rwlock_t);
+    TYPE_INFO(pthread_rwlockattr_t);
+    
+    CONSTANT_INT(PTHREAD_MUTEX_NORMAL);
+    CONSTANT_INT(PTHREAD_MUTEX_ERRORCHECK);
+    CONSTANT_INT(PTHREAD_MUTEX_RECURSIVE);
+    CONSTANT_INT(PTHREAD_MUTEX_DEFAULT);
+    return 0;
+}
