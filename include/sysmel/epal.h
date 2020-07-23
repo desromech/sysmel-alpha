b1c7ea908ebd67b1c9baee39584e2e1d6c5b10f0
Ronie Salgado <roniesalg@gmail.com>
2020-07-23 03:38:21 +0200

    I created additional bindings for some operating system specific apis in the epal, and in win32.

---
+++ include/sysmel/epal.h
@@ -0,0 +1,48 @@
+#ifndef SYSMEL_EPAL_H
+#define SYSMEL_EPAL_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+#define SYSMEL_EPAL_EXPORT extern "C"
+#else
+#define SYSMEL_EPAL_EXPORT
+#endif
+
+typedef struct SysmelArgList_s
+{
+    const char *elements;
+    size_t size;
+} SysmelArgList;
+
+/* Sysmel IO interface. */
+SYSMEL_EPAL_EXPORT intptr_t sysmel_epal_write(int fd, const void *buffer, size_t count);
+SYSMEL_EPAL_EXPORT int sysmel_epal_isatty(int fd);
+
+/* Sysmel native virtual memory interface. */
+SYSMEL_EPAL_EXPORT void* sysmel_epal_reserveAndCommitAddressSpaceWithAlignment(size_t size, size_t alignment);
+SYSMEL_EPAL_EXPORT void* sysmel_epal_reserveAndCommitAddressSpace(size_t size);
+SYSMEL_EPAL_EXPORT void* sysmel_epal_reserveAddressSpace(size_t size);
+SYSMEL_EPAL_EXPORT bool sysmel_epal_commitAddressSpace(void* addressSpace, size_t offset, size_t size);
+SYSMEL_EPAL_EXPORT bool sysmel_epal_releaseAddressSpace(void* addressSpace, size_t offset, size_t size);
+SYSMEL_EPAL_EXPORT bool sysmel_epal_freeAddressSpace(void* addressSpace, size_t size);
+
+/* Sysmel time interface */
+typedef int64_t sysmel_epal_ticks_t;
+#define SYSMEL_TICKS_PER_MICROSECOND 1
+#define SYSMEL_TICKS_PER_MILLISECOND (SYSMEL_TICKS_PER_MICROSECOND*1000)
+#define SYSMEL_TICKS_PER_SECOND (SYSMEL_TICKS_PER_MILLISECOND*1000)
+
+SYSMEL_EPAL_EXPORT sysmel_epal_ticks_t sysmel_epal_microsecondClockValue();
+SYSMEL_EPAL_EXPORT sysmel_epal_ticks_t sysmel_epal_monotonicClockValue();
+SYSMEL_EPAL_EXPORT sysmel_epal_ticks_t sysmel_epal_processClockValue();
+SYSMEL_EPAL_EXPORT sysmel_epal_ticks_t sysmel_epal_threadClockValue();
+SYSMEL_EPAL_EXPORT void sysmel_epal_sleepFor(sysmel_epal_ticks_t time);
+SYSMEL_EPAL_EXPORT void sysmel_epal_sleepUntil(sysmel_epal_ticks_t time);
+
+/* Sysmel main program entry point. */
+SYSMEL_EPAL_EXPORT int sysmel_main(SysmelArgList argList);
+
+#endif /* SYSMEL_EPAL_H */
