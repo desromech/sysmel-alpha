00436036f38ad36a0b63b805e9b40e9c9ab7d734
Ronie Salgado <roniesalg@gmail.com>
2020-08-30 05:04:37 +0200

    I added the third party folders into the packaging scripts.

--- scripts/packageArtifacts.ps1
+++ scripts/packageArtifacts.ps1
@@ -14,4 +14,5 @@ foreach($element in "pharo-vm",
 
 Copy-Item -Recurse -Path "module-sources/*" -Destination "$DIST/module-sources"
 Copy-Item -Recurse -Path "pharo-local/iceberg/**/abstract-gpu/bindings/sysmel/*" -Destination "$DIST/module-sources"
+Copy-Item -Recurse -Path "third-party/*" -Destination "$DIST/third-party/third-party"
 Copy-Item -Recurse -Path "pharo-local/phanapi/libs/abstract-gpu/" -Destination "$DIST/third-party/abstract-gpu"
--- scripts/packageArtifacts.sh
+++ scripts/packageArtifacts.sh
@@ -12,4 +12,5 @@ cp -R pharo-vm pharo pharo-ui $DIST
 cp -R Pharo*.sources sysmel.* sysmelc sysmelc-ui $DIST
 cp -R module-sources/* pharo-local/iceberg/**/abstract-gpu/bindings/sysmel/* $DIST/module-sources
 cp -R pharo-local/phanapi/libs/abstract-gpu/ $DIST/third-party/abstract-gpu
+cp -R third-party/* $DIST/third-party
 cp -R LICENSE README.md docs tests samples $DIST
