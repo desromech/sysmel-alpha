0768c3b013ded886dec0d45b28958dae0a13d5d4
Ronie Salgado <roniesalg@gmail.com>
2020-08-30 19:54:36 +0200

    I fixed some issues in the artifact packaging scripts.

--- scripts/packageArtifacts.ps1
+++ scripts/packageArtifacts.ps1
@@ -3,6 +3,7 @@ $ErrorActionPreference = "Stop"
 $DIST = "artifacts/dist/win64/"
 
 New-Item -Path "$DIST/module-sources" -ItemType "directory"
+New-Item -Path "$DIST/third-party" -ItemType "directory"
 foreach($element in "pharo-vm",
     "docs", "samples", "tests",
     "README.md", "LICENSE",
@@ -15,5 +16,5 @@ foreach($element in "pharo-vm",
 Copy-Item -Recurse -Path "module-sources/*" -Destination "$DIST/module-sources"
 Copy-Item -Recurse -Path "third-party/*" -Destination "$DIST/third-party"
 Copy-Item -Recurse -Path "pharo-local/iceberg/**/abstract-gpu/bindings/sysmel/module-sources/*" -Destination "$DIST/module-sources"
-Copy-Item -Recurse -Path "pharo-local/iceberg/**/abstract-gpu/bindings/sysmel/third-party/*" "$DIST/third-party"
+Copy-Item -Recurse -Path "pharo-local/iceberg/**/abstract-gpu/bindings/sysmel/third-party/*" -Destination "$DIST/third-party"
 Copy-Item -Recurse -Path "pharo-local/phanapi/libs/abstract-gpu/*" -Destination "$DIST/third-party/abstract-gpu"
