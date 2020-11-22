199936dedbd3a566151f1299879bb5d3b3845f0e
Ronie Salgado <roniesalg@gmail.com>
2020-11-22 19:34:38 +0100

    I added the missing support code for win32 GUI application main entry point.

---
+++ native-module-sources/SysmelUWPMain/main.cpp
@@ -0,0 +1,64 @@
+#include <windows.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.Input.h>
+
+using namespace winrt;
+
+using namespace Windows;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Foundation::Numerics;
+using namespace Windows::UI;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Composition;
+
+struct SysmelMainArgs
+{
+    size_t count;
+    const char **elements;
+};
+
+extern "C" int sysmel_main(SysmelMainArgs args);
+
+struct SysmelUWPApp : implements<SysmelUWPApp, IFrameworkViewSource, IFrameworkView>
+{
+    IFrameworkView CreateView()
+    {
+        return *this;
+    }
+
+    void Initialize(CoreApplicationView const &)
+    {
+    }
+
+    void Load(hstring const&)
+    {
+    }
+
+    void Uninitialize()
+    {
+    }
+
+    void SetWindow(CoreWindow const & window)
+    {
+    }
+
+    void Run()
+    {
+        SysmelMainArgs args = {};
+        sysmel_main(args);
+    }
+};
+
+int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+{
+    CoreApplication::Run(make<SysmelUWPApp>());
+}
+
+int __stdcall wmain(int, wchar_t*)
+{
+    CoreApplication::Run(make<SysmelUWPApp>());
+}
