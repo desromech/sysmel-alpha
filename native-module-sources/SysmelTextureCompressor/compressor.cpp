713f06a46fa2000587a3b072dbe15a753cd2bc9e
Ronie Salgado <roniesalg@gmail.com>
2021-03-09 11:10:48 -0300

    I implemented the etc1 compressor for mobile devices. I added a method for extracting a frustum from a standard projection matrix. Minor refactoring in the texture converter tool.

--- native-module-sources/SysmelTextureCompressor/compressor.cpp
+++ native-module-sources/SysmelTextureCompressor/compressor.cpp
@@ -44,6 +44,80 @@ static int getPixelFormatComponents(PixelFormat format)
     }
 }
 
+static void compressEtc1(int sourcePixelSize, int width, int height, int sourcePitch, const uint8_t *sourceData, uint8_t *dest, GLint destPitch, bool addAlpha = false)
+{
+    // FIXME: wrap this call in std::once_flag
+    rg_etc1::pack_etc1_block_init();
+
+    rg_etc1::etc1_pack_params packParams;
+    packParams.m_quality = rg_etc1::cMediumQuality;
+
+    uint8_t readedBlock[4][4][4];
+    uint8_t colorPackBlock[4][4][4];
+
+    auto destRow = dest;
+    auto sourceRow = sourceData;
+    for (int ry = 0; ry < height; ry += 4)
+    {
+        auto blockHeight = (height > ry + 3) ? 4 : height - ry;
+        auto sourcePixel = sourceRow;
+        auto destPixel = destRow;
+        for (int rx = 0; rx < width; rx += 4)
+        {
+            auto blockWidth = (width > rx + 3) ? 4 : width - rx;
+
+            // Clear the readed block data.
+            memset(readedBlock, 0, sizeof(readedBlock));
+
+            // Read the block data.
+            auto blockSourcePixelRow = sourcePixel;
+            for(int j = 0; j < blockHeight; ++j)
+            {
+                auto blockSourcePixel = blockSourcePixelRow;
+                for(int i = 0; i < blockWidth; ++i)
+                {
+                    for(int k = 0; k < sourcePixelSize; ++k)
+                        readedBlock[j][i][k] = blockSourcePixel[k];
+                    blockSourcePixel += sourcePixelSize;
+                }
+
+                blockSourcePixelRow += sourcePitch;
+            }
+
+            // Encode the optional alpha component.
+            if(addAlpha)
+            {
+                *destPixel++ = (readedBlock[0][0][3] >> 4) | (readedBlock[0][1][3] & 0xf0);
+                *destPixel++ = (readedBlock[0][2][3] >> 4) | (readedBlock[0][3][3] & 0xf0);
+                *destPixel++ = (readedBlock[1][0][3] >> 4) | (readedBlock[1][1][3] & 0xf0);
+                *destPixel++ = (readedBlock[1][2][3] >> 4) | (readedBlock[1][3][3] & 0xf0);
+                *destPixel++ = (readedBlock[2][0][3] >> 4) | (readedBlock[2][1][3] & 0xf0);
+                *destPixel++ = (readedBlock[2][2][3] >> 4) | (readedBlock[2][3][3] & 0xf0);
+                *destPixel++ = (readedBlock[3][0][3] >> 4) | (readedBlock[3][1][3] & 0xf0);
+                *destPixel++ = (readedBlock[3][2][3] >> 4) | (readedBlock[3][3][3] & 0xf0);
+            }
+
+            // Copy and clear the alpha for encoding the color data.
+            memcpy(colorPackBlock, readedBlock, sizeof(readedBlock));
+            for(int j = 0; j < 4; ++j)
+            {
+                for(int i = 0; i < 4; ++i)
+                    colorPackBlock[j][i][3] = 255;
+            }
+
+            // Encode the ETC1 color data.
+            rg_etc1::pack_etc1_block(destPixel, reinterpret_cast<const unsigned int*> (readedBlock), packParams);
+
+            // Advance to the next block.
+            sourcePixel += sourcePixelSize*4;
+            destPixel += 8;
+        }
+
+        sourceRow += sourcePitch*4;
+        destRow += destPitch;
+    }
+}
+
 bool sysmel_textureCompressor_compressTextureSlice(
     uint32_t width, uint32_t height, PixelFormat sourceFormat, int32_t sourcePitch, const uint8_t *sourcePixels,
     PixelFormat destFormat, int32_t destPitch, uint8_t *destPixels)
@@ -88,6 +162,14 @@ bool sysmel_textureCompressor_compressTextureSlice(
         sysmel_txc_tx_compress_bcn(getPixelFormatComponents(sourceFormat), width, height,
                   sourcePixels, 5,
                   destPixels, destPitch);
+        return true;
+    case PixelFormat::ETC1_R8G8B8_UNorm:
+    case PixelFormat::ETC1_R8G8B8_UNormSRGB:
+        compressEtc1(getPixelFormatComponents(sourceFormat), width, height, sourcePitch, sourcePixels, destPixels, destPitch, false);
+        return true;
+    case PixelFormat::ETC1_R8G8B8A4_UNorm:
+    case PixelFormat::ETC1_R8G8B8A4_UNormSRGB:
+        compressEtc1(getPixelFormatComponents(sourceFormat), width, height, sourcePitch, sourcePixels, destPixels, destPitch, true);
         return true;
     default:
         return false;
