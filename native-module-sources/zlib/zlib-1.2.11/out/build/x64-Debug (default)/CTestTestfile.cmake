990b69743dc9fc4c1438087fdec35b75fed4bb53
Ronie Salgado <roniesalg@gmail.com>
2021-04-05 22:13:54 -0400

    Use the WBT for building zlib and freetype in Windows instead of bundling precompiled libraries for them.

---
+++ native-module-sources/zlib/zlib-1.2.11/out/build/x64-Debug (default)/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/projects/woden-sysmel/pharo-local/iceberg/ronsaldo/sysmel/native-module-sources/zlib/zlib-1.2.11
+# Build directory: C:/projects/woden-sysmel/pharo-local/iceberg/ronsaldo/sysmel/native-module-sources/zlib/zlib-1.2.11/out/build/x64-Debug (default)
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/projects/woden-sysmel/pharo-local/iceberg/ronsaldo/sysmel/native-module-sources/zlib/zlib-1.2.11/CMakeLists.txt;235;add_test;C:/projects/woden-sysmel/pharo-local/iceberg/ronsaldo/sysmel/native-module-sources/zlib/zlib-1.2.11/CMakeLists.txt;0;")
