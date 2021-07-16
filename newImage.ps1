ac0bde65b672ca91f4f3c300dba05d8bfb9000bf
Ronie Salgado <roniesalg@gmail.com>
2021-07-16 18:28:38 -0400

    I updated the new image scripts to use the released version of Pharo 9.

--- newImage.ps1
+++ newImage.ps1
@@ -1,8 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-$PharoImage_URL = "http://files.pharo.org/get-files/90/pharo64.zip"
-$PharoVM_URL = "http://files.pharo.org/get-files/90/pharo64-win-headless-latest.zip"
-
+$PharoImage_URL = "http://files.pharo.org/get-files/90/pharoImage-x86_64.zip"
+$PharoVM_URL = "http://files.pharo.org/get-files/90/pharo-vm-Windows-x86_64-stable.zip"
 function downloadAndExtractZip {
     $URL = $args[0]
     $DestFileName = $args[1]
--- newImageForCI.sh
+++ newImageForCI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O- https://get.pharo.org/64/90+vmHeadlessLatest | bash || exit 1
+wget -O- https://get.pharo.org/64/90+vm | bash || exit 1
 
 ./pharo Pharo.image st scripts/loadImage.st
